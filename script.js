39f52147c6f6c674b63698dfd7e2e7b9137a9ab4
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-07 16:43:26 -0600

    set method on map

---
+++ script.js
@@ -0,0 +1,9 @@
+const rest = new Map();
+
+rest.set("name", "Classico Italiano");
+
+rest.set(1, "Florence, Italy");
+
+rest.set(2, "Lisbon, Portugal");
+
+rest.set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"]);
