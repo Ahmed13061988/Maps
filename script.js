bf3717a1da0cd12afb1c5fe200d87956b184db1c
Ahmed Hussein <wolktheline@yahoo.com>
2022-09-07 16:45:23 -0600

    chaining set method

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const rest = new Map();
 
+//Set method
 rest.set("name", "Classico Italiano");
-
+//key,  value
 rest.set(1, "Florence, Italy");
-
+//key,  value
 rest.set(2, "Lisbon, Portugal");
 
-rest.set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"]);
+//Chaining set map
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23);
